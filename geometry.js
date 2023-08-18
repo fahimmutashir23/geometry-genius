91e241857bbe1e8dcf2f0894ee206b34a9fde10b
fahimmutashir23 <taslimit23@gmail.com>
2023-08-19 00:32:30 +0600

    press enter and submit added

--- geometry.js
+++ geometry.js
@@ -13,7 +13,7 @@ function setResult(result , calculateValue){
 }
 //-----------------------------------triangle calculate---------------------------------------//
 
-document.getElementById("triangleBtn").addEventListener('click', function(){
+function enterTryangleHit(){
     const triangleBase = getInputValue("triangleInputBase")
     const tritangleHight = getInputValue("triangleInputHight")
 
@@ -26,94 +26,142 @@ document.getElementById("triangleBtn").addEventListener('click', function(){
     
     setResult('triangleResult', calculate)
     setAreaCalculator("Triangle", calculate)
+}
+document.getElementById("triangleBtn").addEventListener('click', enterTryangleHit)
+
+document.getElementById('triangleInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterTryangleHit()
+    }
 })
 
 //------------------------------------rectangle calculate-------------------------------------//
 
-document.getElementById('rectangleBtn').addEventListener('click', function(){
+function enterRectangleHit(){
     const rectangleWidth = getInputValue("rectangleInputWidth");
     const rectangleHight = getInputValue("rectangleInputHight");
-
+    
     if(isNaN(rectangleHight) || isNaN(rectangleWidth)){
         return alert("please enter a number")
     }
-
+    
     const rectangleTotal = rectangleHight * rectangleWidth;
     const calculateRectangle = rectangleTotal.toFixed(2)
     
     setResult('rectangleResult', calculateRectangle)
     setAreaCalculator("Rectangle", calculateRectangle)
+}
+
+document.getElementById('rectangleBtn').addEventListener('click', enterRectangleHit)
+
+document.getElementById('rectangleInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterRectangleHit()
+    }
 })
 
 //---------------------------------parallelogram calculate------------------------------------//
 
-document.getElementById("parallelogramBtn").addEventListener('click', function(){
+function enterParallelogramHit(){
+
     const parallelogramBase = getInputValue("parallelogramInputBase");
     const parallelogramWidth = getInputValue("parallelogramInputHight");
-
+    
     if(isNaN(parallelogramBase) || isNaN(parallelogramWidth)){
         return alert("please enter a number")
     }
-
+    
     const parallelogramTotal = parallelogramBase * parallelogramWidth;
     const calculateparallelogram = parallelogramTotal.toFixed(2)
-
+    
     setResult('parallelogramResult', calculateparallelogram)
-
+    
     setAreaCalculator("Parallelogram", calculateparallelogram)
+}
+document.getElementById("parallelogramBtn").addEventListener('click', enterParallelogramHit)
+
+document.getElementById('parallelogramInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterParallelogramHit()
+    }
 })
 
 //---------------------------------rhombus calculate------------------------------------//
 
-document.getElementById('rhombusBtn').addEventListener('click', function(){
+function enterRhombusHit(){
+
     const rhombusBase = getInputValue("rhombusInputBase");
     const rhombusHight = getInputValue('rhombusInputHight');
-
+    
     if(isNaN(rhombusBase) || isNaN(rhombusHight)){
         return alert("please enter a number")
     }
-
+    
     const rhombusTotal = rhombusBase * rhombusHight;
     const calculateRhombus = rhombusTotal.toFixed(2)
     setResult("rhombusResult", calculateRhombus)
-
+    
     setAreaCalculator("Rhombus", calculateRhombus)
+}
+document.getElementById('rhombusBtn').addEventListener('click', enterRhombusHit)
+
+document.getElementById('rhombusInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterRhombusHit()
+    }
 })
 
 //---------------------------------pentagon calculate------------------------------------//
 
-document.getElementById("pentagonBtn").addEventListener('click', function(){
+function enterPentagonHit(){
+
     const pentagonBase = getInputValue('pentagonInputBase');
     const pentagonHight = getInputValue('pentagonInputHight');
-
+    
     if(isNaN(pentagonBase) || isNaN(pentagonHight)){
         return alert("please enter a number")
     }
-
+    
     const pentagonTotal = 0.5 * pentagonBase * pentagonHight;
     const calculatepentagon = pentagonTotal.toFixed(2)
-
+    
     setResult("pentagonResult", calculatepentagon)
-
+    
     setAreaCalculator("Pentagon", calculatepentagon)
+}
+document.getElementById("pentagonBtn").addEventListener('click', enterPentagonHit)
+
+document.getElementById('pentagonInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterPentagonHit()
+    }
 })
 
 //---------------------------------ellipse calculate------------------------------------//
 
-document.getElementById("ellipseBtn").addEventListener('click', function(){
+function enterEllipseHit(){
+
     const ellipseBase = getInputValue("ellipseInputBase");
     const ellipseHight = getInputValue("ellipseInputHight");
-
+    
     if(isNaN(ellipseBase) || isNaN(ellipseHight)){
         return alert("please enter a number")
     }
     const ellipseTotal = 3.1416 * ellipseBase * ellipseHight;
     const calculateEllipse = ellipseTotal.toFixed(2)
-
+    
     setResult('ellipseResult', calculateEllipse)
     setAreaCalculator("Ellipse", calculateEllipse,)
+}
+document.getElementById("ellipseBtn").addEventListener('click', enterEllipseHit)
+
+document.getElementById('ellipseInputHight').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        enterEllipseHit()
+    }
 })
 
+//-------------------------------area calculate-----------------------------------------------//
 
 function setAreaCalculator(elementName, elementValue){
     const areaCalculator = document.getElementById('area-calculator');
